dd1f2b5ce194dbc79737387a51ac2d1a9f8700c6
lanmanul <gatecrash3r@gmail.com>
2025-09-09 23:21:21 +0300

    L1.8 v1.1

--- L1/8/main.go
+++ L1/8/main.go
@@ -4,28 +4,34 @@ import (
 	"fmt"
 )
 
-// setBit устанавливает i-й бит числа n в значение bit (0 или 1)
-func setBit(n int64, i uint, bit int) int64 {
-	if bit == 1 {
-		// Устанавливаем i-й бит в 1 (операция OR)
-		n |= 1 << (i - 1)
-	} else {
-		// Сбрасываем i-й бит в 0 (операция AND NOT)
-		n &^= 1 << (i - 1)
-	}
-	return n
-}
-
 func main() {
-	var num int64 = 5 // 0101₂
+	var num int64
+	var bitVal int
+
+	// Просим ввести число
+	fmt.Print("Введите число: ")
+	fmt.Scan(&num)
+
+	// Просим ввести значение для 1-го бита (0 или 1)
+	for {
+		fmt.Print("Введите новое значение для 1-го бита (0 или 1): ")
+		fmt.Scan(&bitVal)
+
+		if bitVal == 0 || bitVal == 1 {
+			break
+		}
+		fmt.Println("Ошибка: нужно ввести только 0 или 1.")
+	}
 
 	fmt.Printf("Исходное число: %d (%04b)\n", num, num)
 
-	// Установим 1-й бит в 0
-	res1 := setBit(num, 1, 0)
-	fmt.Printf("Установка 1-го бита в 0: %d (%04b)\n", res1, res1)
+	if bitVal == 0 {
+		// Сбрасываем младший бит
+		num = num &^ 1
+	} else {
+		// Устанавливаем младший бит
+		num = num | 1
+	}
 
-	// Установим 2-й бит в 1
-	res2 := setBit(num, 2, 1)
-	fmt.Printf("Установка 2-го бита в 1: %d (%04b)\n", res2, res2)
+	fmt.Printf("Результат: %d (%04b)\n", num, num)
 }
