56fbda0c8c601047e4ab23e9a15b47033b740d6e
lanmanul <gatecrash3r@gmail.com>
2025-09-20 16:47:14 +0300

    L1.10 v1.0

--- L1/10/main.go
+++ L1/10/main.go
@@ -6,16 +6,20 @@ import (
 )
 
 func main() {
+
+	//Задаём массив температур
 	temps := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
 
 	groups := make(map[int][]float64)
 
 	for _, t := range temps {
+		//Для каждой температуры определяем ключ группы
 		key := int(math.Floor(t/10.0) * 10)
+		//Добавляем температуру в groups
 		groups[key] = append(groups[key], t)
 	}
 
-	// Вывод результата
+	// Выводим результат
 	for k, v := range groups {
 		fmt.Printf("%d: %v\n", k, v)
 	}
